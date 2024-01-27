b50c8c7c34f539b900b1a22c30f3ea49e0a84583
Nofated095 <nofated095@outlook.com>
2024-01-28 00:02:36 +0800

    update css

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function SwitchNav(value) {
 
 function NaviBar() {
   return (
-    <mdui-navigation-bar value="navigation">
+    <mdui-navigation-bar value="home">
       <mdui-navigation-bar-item icon="home--outlined" value="home">Home</mdui-navigation-bar-item>
       <mdui-navigation-bar-item icon="feed--outlined" value="about">About</mdui-navigation-bar-item>
       <mdui-navigation-bar-item icon="edit--outlined" value="blog" href="https://blog.amane.icu" target="_blank">Blog</mdui-navigation-bar-item>
@@ -35,13 +35,13 @@ export default function App() {
   return (
     <>
   
-  <div class="title mdui-prose">
+  <div class="title">
       <img loading='lazy' src="https://cfdn.nofated.win/avatars/avatar.webp" class='avatar'/>
       <h1>Amane</h1>
     </div>
 
 
-    <div class="home mdui-prose">
+    <div class="home">
       <h2>Yoo!</h2>
       <p>I'm Amane, aka Nofated.</p>
 
@@ -56,11 +56,14 @@ export default function App() {
 
 
 
-    <div class="about mdui-prose">
-      2020022
-
+    <div class="about">
+      <h2>About</h2>
+      <p>My full name is Tanikaze Amane, but you may hear some one call me Nofated or nof.</p>
+      <p>I do open-source works, you can check them on <a href="https://github.com/Nofated095" target="_blank">GitHub</a>. Most of them are useless and have lots of bugs.</p>
 
     </div>
+
+
 
 
 
